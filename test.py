b8335a64c9594ef6f44964641b3b057c645cb66a
Baroxyton <69308275+baroxyton@users.noreply.github.com>
2025-12-10 15:58:33 +0100

    fix

--- test.py
+++ test.py
@@ -7,5 +7,5 @@ mp.on("message", lambda peer, data: print(f"Got message: {data}"))
 mp.on("peer_connected", lambda peer: print(f"Peer connected: {peer.peer_id}"))
 mp.on("all_peers_connected", lambda: print("All peers connected!"))
 await mp.start_server()
-await mp.seek_peers(3)  # Seek 3 peers
+await mp.seek_peers(2)  # Seek 2 peers
 mp.emit("message", {"text": "Hello everyone!"})  # Send to all peers
